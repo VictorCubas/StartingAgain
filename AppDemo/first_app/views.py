eebbadf230dcc5da442e58b60d9dcd44e80d93e8
Victor Cubas <vhcubas91@gmail.com>
2022-08-04 13:59:31 -0400

    working on views

--- AppDemo/first_app/views.py
+++ AppDemo/first_app/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from os import curdir
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Curso
 # Create your views here.
 
-def index(request):
-	return HttpResponse('<h1>Hello world!</h1>')
+def home(request):
+	#return HttpResponse('<h1>Hello world!</h1>')
+	cursosListado = Curso.objects.all()
+	return render(request, 'gestionCurso.html', {'cursos':cursosListado})
+
+def registrarCurso(request):
+	codigo = request.POST['txtCodigo']
+	nombre = request.POST['txtNombre']
+	creditos = request.POST['numCreditos']
+
+	curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+	return redirect('/')
